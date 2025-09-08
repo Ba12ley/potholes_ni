6a3a4d55ddf969e73dff396aa4a6850369de9b00
Dornar <59340405+Ba12ley@users.noreply.github.com>
2025-09-08 20:04:58 +0100

    more styling

--- views/web_view.py
+++ views/web_view.py
@@ -1,7 +1,9 @@
 import json
 
 import fastapi
-from ipyleaflet import Map, GeoJSON
+from ipyleaflet import (
+    Map, GeoJSON, AwesomeIcon
+)
 from ipywidgets.embed import embed_minimal_html
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -12,9 +14,14 @@ router = fastapi.APIRouter()
 
 
 def make_map(filename='map.html'):
-    m = Map(center=(54.65, -6.9), zoom=8)
+    m = Map(center=(54.65, -6.9), zoom=8, scroll_wheel_zoom=True, touch_zoom=True, dragging=True, tap=True,
+            tap_tolerance=15, world_copy_jump=False, close_popup_on_click=True, bounce_at_zoom_limits=True,
+            keyboard=True, keyboard_pan_delta=80, keyboard_zoom_delta=1, inertia=True, inertia_deceleration=3000,
+            inertia_max_speed=1500, zoom_control=True, attribution_control=True)
     with open('data/potholes.geojson') as f:
         data = json.load(f)
+
+    pothole_icon = AwesomeIcon(name='warning', marker_color='orange')
 
     style = {
         "color": "red",
@@ -24,7 +31,8 @@ def make_map(filename='map.html'):
         "opacity": 0.6,
         "fillOpacity": 0.6,
     }
-    geo_json = GeoJSON(data=data, style=style)
+    geo_json = GeoJSON(data=data, style=style, point_style=pothole_icon)
+    m.geojson = geo_json
     m.add(geo_json)
     embed_minimal_html(filename, views=[m], title='Potholes NI')
 
